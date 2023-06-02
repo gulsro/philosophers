c2df7857e045e8fa36ab93d5d74946c82fb259e1
yetkin <glserozkan@gmail.com>
2023-06-02 14:13:01 +0200

    eating and forks taken

--- actions.c
+++ actions.c
@@ -1,6 +1,30 @@
 #include "philo.h"
 
-void	thinking(t_diner)
+void	taking_forks(t_diner *diner)
+{
+	if (diner->philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(diner->fork[diner->philo->id]);
+		pthread_mutex_lock(diner->fork[diner->philo->id + 1]);
+		pthread_mutex_lock(diner->print);
+		printf("%ld %d has taken a fork\n", elapsed_time, diner->philo->id);
+		pthread_mutex_unlock(diner->print);
+	}
+}
+
+void	eating(t_diner *diner)
+{
+	pthread_mutex_lock(diner->print);
+	printf("%ld %d is eating\n", elapsed_time, diner->philo->id);
+	pthread_mutex_unlock(diner->print);
+	diner->philo->eaten_meals++;
+	diner->philo->last_meal_time = get_current_time();
+	usleep(time_to_eat);
+	pthread_mutex_unlock(diner->fork[diner->philo->id]);
+	pthread_mutex_unlock(diner->fork[diner->philo->id + 1]);
+}
+
+void	thinking(t_diner *diner)
 {
 	long	elapsed_time;
 	long	now;
@@ -9,11 +33,11 @@ void	thinking(t_diner)
 	elapsed_time = elapsed_time(now);
 	pthread_mutex_lock(&diner->print);
 	printf("%ld %d is thinking\n", elapsed_time, diner->philo->id);
+	printf("%ld %d is thinking\n", elapsed_time, diner->philo->id);
 	pthread_mutex_unlock(&diner->print);
 }
 
-//still some issues with usleep. after of before print???
-void    sleeping(t_diner)
+void    sleeping(t_diner *diner)
 {
         long    elapsed_time;
         long    now;
@@ -23,4 +47,5 @@ void    sleeping(t_diner)
         pthread_mutex_lock(&diner->print);
         printf("%ld %d is sleeping\n", elapsed_time, diner->philo->id);
         pthread_mutex_unlock(&diner->print);
+	usleep(diner->time_to_sleep);
 }
